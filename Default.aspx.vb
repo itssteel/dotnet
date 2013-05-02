07e826692375f7f703cf0346a5a581c5e1e377d3
itssteel <its.tatasteel@gmail.com>
2013-05-02 21:41:31 +0530

    Update Default.aspx.vb

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,8 +1,7 @@
-
 Partial Class _Default
     Inherits System.Web.UI.Page
 
     Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Label1.Text = "Hello IAS"
+        Label1.Text = "Hello IAS BABA"
     End Sub
 End Class
